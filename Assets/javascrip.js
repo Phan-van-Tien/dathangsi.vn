b586301a8952f7845b233b95aacdc8740f115637
Phan Van Tien <tien.phan15041999@gmail.com>
2025-09-04 10:31:46 +0700

    first comment

---
+++ Assets/javascrip.js
@@ -0,0 +1,42 @@
+// trược menu ẩn khi click vào bar menu
+var menu_an = document.querySelector(".menu_an")
+function hien_menu_an() {
+    menu_an.classList.toggle("open1");
+}
+// trược menu ẩn khi click vào bar menu
+var menu__992px_container = document.querySelector(".menu__992px_container")
+function hien_menu_an__992px() {
+    menu__992px_container.classList.toggle("open");
+}
+var menu_992px__sanphammoiul = document.querySelector(".menu_992px__sanphammoiul")
+function hien_menu_an__992px__sanphammoiul() {
+    menu_992px__sanphammoiul.classList.toggle("open");
+}
+var menu_992px__thoitrangul = document.querySelector(".menu_992px__thoitrangul")
+function hien_menu_an__992px__thoitrangul() {
+    menu_992px__thoitrangul.classList.toggle("open");
+}
+var menu_992px__thietbiul = document.querySelector(".menu_992px__thietbiul")
+function hien_menu_an__992px__thietbiul() {
+    menu_992px__thietbiul.classList.toggle("open");
+}
+var menu_992px__chamsocul = document.querySelector(".menu_992px__chamsocul")
+function hien_menu_an__992px__chamsocul() {
+    menu_992px__chamsocul.classList.toggle("open");
+}
+var menu_992px__dodungcul = document.querySelector(".menu_992px__dodungcul")
+function hien_menu_an__992px__dodungul() {
+    menu_992px__dodungcul.classList.toggle("open");
+}
+var menu_992px__dodung2ul = document.querySelector(".menu_992px__dodung2ul")
+function hien_menu_an__992px__dodung2ul() {
+    menu_992px__dodung2ul.classList.toggle("open");
+}
+var menu_992px__dogiadungul = document.querySelector(".menu_992px__dogiadungul")
+function hien_menu_an__992px__dogiadungul() {
+    menu_992px__dogiadungul.classList.toggle("open");
+}
+var menu_992px__linhkienul = document.querySelector(".menu_992px__linhkienul")
+function hien_menu_an__992px__linhkienul() {
+    menu_992px__linhkienul.classList.toggle("open");
+}
